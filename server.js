6648979b2da078a29033d0188b9f7fca882701f0
yuuuri <yuuuri@yahoo.com>
2015-03-24 08:00:42 -0700

    function result and random choice is added and reused from hw5

--- server.js
+++ server.js
@@ -6,7 +6,93 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
     http = require("http"),
-    app = express();
+    app = express(),
+    outcome,
+    wins = 0,
+    ties = 0,
+    losses = 0;
+
+
+// reuse from hw5
+// random number generator for rock, paper, scissors, lizard, spock
+function randomChoice() {
+    var choice = ["rock", "paper", "scissors", "lizard", "spock"],
+        random = Math.floor(Math.random() * 5);
+    return choice[random];
+}
+
+// reuse from hw5
+// called by app.post, generates serverChoice returns outcome and update
+// results
+function result(playerChoice) {
+    var serverChoice = randomChoice();
+    console.log("******   SERVER SIDE   *******");
+    console.log("PLAYER CHOICE: " + playerChoice);
+    console.log("SERVER CHOICE: " + serverChoice);
+    if (playerChoice === "rock") {
+        if ((serverChoice === "scissors") || (serverChoice === "lizard")) { //player wins
+            outcome = 'win';
+            wins++;
+        } else if (serverChoice === "rock") { //player ties with server
+            outcome = 'tie';
+            ties++;
+        } else { //otherwise player loses
+            outcome = 'lose';
+            losses++;
+        }
+    }else if (playerChoice === "paper") {
+         if ((serverChoice === "rock") || (serverChoice === "spock")) {
+            outcoe ='win';
+            wins++;
+        } else if (serverChoice === "paper") {
+            outcoe ='tie';
+            ties++;
+        } else {
+            outcome ='lose';
+            losses++;
+        }   
+    }else if (playerChoice === "scissors") {
+         if ((serverChoice === "lizard") || (serverChoice === "paper")) {
+            outcome ='win';
+            wins++;
+        } else if (serverChoice === "scissors") {
+            outcoe ='tie';
+            ties++;
+        } else {
+            outcome = 'lose';
+            losses++;
+        }    
+    }else if (playerChoice === "lizard") {
+         if ((serverChoice === "spock") || (serverChoice === "paper")) {
+            outcoe ='win';
+            wins++;
+        } else if (serverChoice === "lizard") {
+            outcome = 'tie';
+            ties++;
+        } else {
+            outcome = 'lose';
+            losses++;
+        }    
+    }else if (playerChoice === "spock") {
+         if ((serverChoice === "scissors") || (serverChoice === "rock")) {
+            outcome ='win';
+            wins++;
+        } else if (serverChoice === "spock") {
+            outcome = 'tie';
+            ties++;
+        } else {
+            outcome = 'lose';
+            losses++;
+        }    
+    }else{
+         res.write("{\"error\" : \"happened in result function\"}");
+         console.log("error happend in result function...");
+    }
+
+} // end of result function
+
+
+
 
 /*
  * body-parser is a piece of express middleware that reads
@@ -37,9 +123,12 @@ app.get("/test", function(req, res) {
 app.post('/play/rock',function(req, res) {
     //var rock = req.body.move;
 
+    result('rock');
+
+
     // create javascript object, object will be sent back to 
     // client side
-    var obj = { outcome: 0, wins: 0, losses: 0, ties: 0};    
+    var obj = { outcome: '', wins: 0, losses: 0, ties: 0};    
         obj.outcome = outcome,
         obj.wins = wins,
         obj.losses = losses,
@@ -49,7 +138,7 @@ app.post('/play/rock',function(req, res) {
         console.log(obj);
         
         
-    )};
+});
 
 
 
