e91bb76761949383f91d6c45e024b46da1104189
yuuuri <yuuuri@yahoo.com>
2015-03-24 09:05:11 -0700

    rest of the click function with ajax is added on app.js

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,8 +1,6 @@
 var main = function() {
 
 
-
-
 	$('#rock').click(function(){
 		//alert("test: rock button works");
 		$.ajax( {
@@ -15,18 +13,108 @@ var main = function() {
 			// up to here user clicked rock button and sent to server
 			// then waits
 
-			success: function(data) {
-				
-        // client receives json object and put it out on browser 
-        $('#result').html(JSON.stringify(data, null, '\t'));
-				console.log(data);
+          success: function(data) {
+            
+            // client receives json object and put it out on browser 
+            // console.log(data)
+            $('#result').html(JSON.stringify(data, null, '\t'));
+          }
 
-			}
-
-		}
-
-		);
+		});
 	});
+
+  //okay, rock button works, just copy and paste the rest of the buttons
+
+  	$('#paper').click(function(){
+		//alert("test: rock button works");
+		$.ajax( {
+			url: '/play/paper',
+			method: 'post',
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			data: 'move=paper',
+
+			// up to here user clicked rock button and sent to server
+			// then waits
+
+          success: function(data) {
+            
+            // client receives json object and put it out on browser 
+            // console.log(data)
+            $('#result').html(JSON.stringify(data, null, '\t'));
+          }
+
+		});
+	});
+
+	$('#scissors').click(function(){
+		//alert("test: rock button works");
+		$.ajax( {
+			url: '/play/scissors',
+			method: 'post',
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			data: 'move=scissors',
+
+			// up to here user clicked rock button and sent to server
+			// then waits
+
+          success: function(data) {
+            
+            // client receives json object and put it out on browser 
+            // console.log(data)
+            $('#result').html(JSON.stringify(data, null, '\t'));
+          }
+
+		});
+	});
+
+	$('#lizard').click(function(){
+		//alert("test: rock button works");
+		$.ajax( {
+			url: '/play/lizard',
+			method: 'post',
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			data: 'move=lizard',
+
+			// up to here user clicked rock button and sent to server
+			// then waits
+
+          success: function(data) {
+            
+            // client receives json object and put it out on browser 
+            // console.log(data)
+            $('#result').html(JSON.stringify(data, null, '\t'));
+          }
+
+		});
+	});
+
+	$('#spock').click(function(){
+		//alert("test: rock button works");
+		$.ajax( {
+			url: '/play/spock',
+			method: 'post',
+			contentType: 'application/x-www-form-urlencoded',
+			dataType: 'json',
+			data: 'move=spock',
+
+			// up to here user clicked rock button and sent to server
+			// then waits
+
+          success: function(data) {
+            
+            // client receives json object and put it out on browser 
+            // console.log(data)
+            $('#result').html(JSON.stringify(data, null, '\t'));
+          }
+
+		});
+	});
+
+
+
 
 
 
