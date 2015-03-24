9440fd2e1ddf9ba1585f132a2e616f80dc4cc6b3
yuuuri <yuuuri@yahoo.com>
2015-03-24 08:11:47 -0700

    ajax for rok's success function is added to paste result on to html

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,7 +1,25 @@
 var main = function() {
 
 	$('#rock').click(function(){
-		alert("testing this button rock works");
+		//alert("testing this button rock works");
+
+    $.ajax ( {
+        url: '/play/rock',
+        method: 'post',
+        contentType: 'application/x-www-form-urlencoded',
+        dataType: 'json',
+        data: 'move=rock',
+
+        // up to here, user clicked rock button and send to
+        // server, then waits
+
+        success: function(data) {
+            //this data will be sent to client on browser
+            $('#result').html(JSON.stringify(data, null, '\t'));
+        }
+
+    }
+    );
 
 	});
 
